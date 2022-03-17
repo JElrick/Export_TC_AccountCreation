37817c7e7100b6ecef8c85cad24f22b834efe814
JElrick <james.elrick@rs-components.com>
2022-03-17 15:04:44 +0000

    Added All Scripts
    added all scripts to tests

--- tests/MT_AC.js
+++ tests/MT_AC.js
@@ -5,16 +5,17 @@ var AccountName = "TestCafe" + dmt.getTime();
 var p = "meowbrowncats1!"
 var e = "RPlan.BSupport@rs-components.com"
 
-fixture `Account Creation - Export`
+fixture `Account Creation - Malta`
 
-    .page `https://export-uat.rsdelivers.com`
+    .page `https://MT-uat.rsdelivers.com`
 
     .meta({author: 'James Elrick', creationDate: '10/02/2021'});
 
   test('Export', async t => {
     
     await t
-    .maximizeWindow()
+
+    //.maximizeWindow()
     .wait(5000)
     //click account button
     .click(Selector('#root > div > header > div.card-component-module_card-component__1NA2E.page-content.header-menu-component_header-card__3KF4p.card-component-module_padding__51wm7.card-component-module_white__2avgM > div > div.header-menu-component_nav-bar__1Ur-s.row > div > div.column.header-menu-component_icon-button__2n1dO.header-menu-component_account__1fdJM'))
@@ -27,8 +28,8 @@ fixture `Account Creation - Export`
     .typeText('#txtPasswordConfirm',p)
     .typeText('#txtEmail',e)
 
-    .click(Selector('#ddlCountry'))
-      .click(Selector('#ddlCountry > option:nth-child(186)'))
+    //.click(Selector('#ddlCountry'))
+    //  .click(Selector('#ddlCountry > option:nth-child(186)'))
     
     .click(Selector('#cmbJobRoles'))
       .click(Selector('#cmbJobRoles > option:nth-child(11)'))
@@ -41,12 +42,10 @@ fixture `Account Creation - Export`
     .click(Selector('#pagecell > form > div > div:nth-child(1) > div.formFields > div.regButtonDiv > div > span > input'))
 
     //.expect(Selector('#root > div > header > div.card-component-module_card-component__1NA2E.page-content.header-menu-component_header-card__3KF4p.card-component-module_padding__51wm7.card-component-module_white__2avgM > div > div.header-menu-component_nav-bar__1Ur-s.row > div > div.column.header-menu-component_icon-button__2n1dO.header-menu-component_account__1fdJM > div.header-menu-component_info__2Ai5g > span').textContent).contains('Hi')
-
+    .wait(2000)
     //click account button
     .click(Selector('#root > div > header > div.card-component-module_card-component__1NA2E.page-content.header-menu-component_header-card__3KF4p.card-component-module_padding__51wm7.card-component-module_white__2avgM > div > div.header-menu-component_nav-bar__1Ur-s.row > div > div.column.header-menu-component_icon-button__2n1dO.header-menu-component_account__1fdJM'))
-
+    .wait(2000)
     //logout
     .click(Selector('#aLogout'))
-    //test homepage
-    .expect(Selector('#root > div > div.page-content-wrapper > div > div > div:nth-child(3) > div > div > div > p.styled-text-component-module_styled-text-component__1Pric.styled-text-component-module_text-size-paragraph__2PEy7.styled-text-component-module_red__RD_RH.styled-text-component-module_mobile-red__1fxte').textContent).contains('RS Export Solutions')
 });
